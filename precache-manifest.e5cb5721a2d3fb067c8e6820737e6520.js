b60b38a91344d5a9ae2f2918355cfa99f5342164
Anim Yusufbhai Malvat <malvat@uwindsor.ca>
2020-03-19 04:36:15 -0400

    Updates

---
+++ precache-manifest.e5cb5721a2d3fb067c8e6820737e6520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb82ea0abd7031b555c168740f1ff73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac63e8c7a415db2a3bf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65d4acbf6701adaa2939",
+    "url": "/static/js/2.0a57f424.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/static/js/2.0a57f424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac63e8c7a415db2a3bf",
+    "url": "/static/js/main.babf9644.chunk.js"
+  },
+  {
+    "revision": "f7ca70934779eb1a963c",
+    "url": "/static/js/runtime-main.1ecca8b7.js"
+  }
+]);
